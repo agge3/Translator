4632d5a1cdbf90672800903bf9835cb0b7ffdfc0
agge3 <129739968+agge3@users.noreply.github.com>
2023-05-12 11:36:11 -0700

    Create Translator.java

---
+++ Translator.java
@@ -0,0 +1,71 @@
+public class Translator {
+
+public static String engToJpWord(String engWord) {
+switch(engWord) {
+case "hello":
+case "yes":
+return("hai");
+break;
+case "no":
+return("iie");
+break;
+case "please":
+return("kudasai");
+break;
+case "airport":
+case "taxi":
+case "home":
+return("uchi");
+break;
+case "university":
+return("daigaku");
+case "small":
+return("chisai");
+break;
+case "large":
+return(ookii);
+break;
+case "japanese":
+return("nihongo");
+break;
+case "english":
+return("eego");
+break;
+case "american":
+return("americajin");
+break;
+case "apartmemt":
+return("apaato");
+break;
+case "supermarket":
+return("suupaa");
+break;
+public static String engToJpPhrase(String engPhrase) {
+switch(engPhrase) {
+case "how are you":
+return("genkidesuka");
+break;
+case "wait a second":
+return("chotta mate");
+break;
+case "see you later":
+return("jyaa, mata");
+break;
+case "goodbye":
+return("sayounara");
+break;
+case "i see":
+return("soudesuka");
+break;
+case "good morning":
+return("ohaiyougozaimasu");
+break;
+case "i'm going to eat dinner":
+return("bangohan o tabemasu");
+break;
+case "how do you say it in english":
+return(
+case "what time is it":
+return("nanji");
+break;
+
